66e5a9ce3cf2c23a5c728e8e3a3ba7987b361ad0
yusof_antar <youssof.antar@gmail.com>
2024-07-10 14:24:27 +0300

    Uodate Title

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/public/brainwave/",
+  base: "/public/brainwave_website/",
   plugins: [react()],
 })
